2df818e9ef1fbe10d2928f0f70a0f8d6c9b6f5eb
hyunzongu <165872785+hyunzongu@users.noreply.github.com>
2025-04-30 13:41:35 +0900

    Update U-05.sh

--- Uscript/U-05.sh
+++ Uscript/U-05.sh
@@ -5,13 +5,13 @@ cat << EOF
 EOF
 
 ROOTPERM="drwx------"
-CHECKPERM=`ls -l /../ | awk '{print $1" "$8" "$9}' | grep root | awk '{print $1}'`    # 여기 퍼미션을 확인하는 코드를 완성하세요
+CHECKPERM=`ls -l /../ | awk '{print $1" "$8" "$9}' | grep root | awk '{print $1}'`   
 
 echo "Recommand root directory permission : "${ROOTPERM}
 echo "Checked   root directory permission : "${CHECKPERM}
 echo ""
 
-if [ ${CHECKPERM} = ${ROOTPERM} ]; then    # 여기 비교하는 구문을 완성하세요 
+if [ ${CHECKPERM} = ${ROOTPERM} ]; then    
         echo "ROOT directory permission OK!"
 else
         echo "ROOT directory permission BAD!!"
